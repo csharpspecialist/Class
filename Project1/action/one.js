8781642ce9170e5d63263129bf7c8d041797796c
csharpspecialist <netspec1@hotmail.com>
2018-03-04 15:56:37 -0500

    added a click event to the one.js file

---
+++ Project1/action/one.js
@@ -0,0 +1,10 @@
+
+let workButton = document.getElementById('good');
+// let smile = function() {
+
+// }
+
+workButton.addEventListener('click', function() {
+    alert("Good times people!!!")
+})
+
